85f37da4d4cc59cc3f22d67f306fe9a3f48388bc
parallela <lubomir.c.stankov@gmail.com>
2020-11-18 13:21:16 +0200

    Fixing react-native-webview library error

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react'
-import {View, WebView } from 'react-native'
+import { View } from 'react-native'
+import { WebView } from "react-native-webview";
 
 
 export const ParticlesView = (props) => {
-    return (
-      <View style={styles.containerStyle}>
-        <WebView
-                    source={require('./widget/particles.html')}
-                    style={{ flex: 1, zIndex: 0 }}/>
-        <View   style={[{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, zIndex: 1 },props.containerStyle]}>
-             {props.children}
-        </View>
+  return (
+    <View style={styles.containerStyle}>
+      <WebView
+        source={require('./widget/particles.html')}
+        style={{ flex: 1, zIndex: 0 }} />
+      <View style={[{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, zIndex: 1 }, props.containerStyle]}>
+        {props.children}
       </View>
-    );
-  };
+    </View>
+  );
+};
 
-  const styles = {
-    containerStyle: {
-    flex:1
-    }
-  };
+const styles = {
+  containerStyle: {
+    flex: 1
+  }
+};
 
 
 
-  
+
 
